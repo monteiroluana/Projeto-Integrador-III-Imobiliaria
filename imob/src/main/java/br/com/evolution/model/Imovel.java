34bb72bec330e26d7a9482cf6380e60ac5ffaa4b
Bruna Sayuri <bruna_susuke@hotmail.com>
2018-05-02 21:46:59 -0300

    model imovel

---
+++ imob/src/main/java/br/com/evolution/model/Imovel.java
@@ -0,0 +1,37 @@
+package br.com.evolution.model;
+
+import java.util.Date;
+
+public class Imovel {
+    private int idImovel;
+    private String codImovel;
+    
+    private Date dataCad;
+    
+    private String servico;
+    private String categoria;
+    private String tipo;
+    
+    private int quartos;
+    private int banheiros;
+    private int suites;
+    private int vagasGaragem;
+    
+    private double areaUtil;
+    private double areaTotal;
+    
+    private String informacao;
+    
+    private String cep;
+    private String rua;
+    private String bairro;
+    private String cidade;
+    private String uf;
+    private String num;
+    private String complemento;
+    
+    private double valor;
+    private double iptu;
+    private double condominio;
+    private String status;
+}
