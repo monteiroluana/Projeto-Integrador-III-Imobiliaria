f3f00afe8da9dccad33a3f67184487c18212f668
Jonas Ribeiro <jonas.aribeiro@Outlook.com>
2018-05-15 17:24:53 -0300

    model Contrato

---
+++ imob/src/main/java/br/com/devolution/model/Contrato.java
@@ -0,0 +1,112 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.devolution.model;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+public class Contrato {    
+    
+    String codContrato;
+    int idImovel;
+    int idCliente;
+    int idUsuario;
+    
+    Date dataContrato;
+    Date dataInicial;
+    Date dataFinal;    
+    
+    String corretor;
+    String tipoContrato;
+    
+    public String getCodContrato() {
+        return codContrato;
+    }
+
+    public void setCodContrato(String codContrato) {
+        this.codContrato = codContrato;
+    }
+
+    public int getIdImovel() {
+        return idImovel;
+    }
+
+    public void setIdImovel(int idImovel) {
+        this.idImovel = idImovel;
+    }
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public Date getDataContrato() {
+        return dataContrato;
+    }
+
+    public void setDataContrato(String dataContrato) {
+        DateFormat formatador = new SimpleDateFormat("dd/MM/yyyy");
+
+        try {
+            this.dataContrato = formatador.parse(dataContrato);
+        } catch (ParseException e) {
+
+        }
+    }
+
+    public Date getDataInicial() {
+        return dataInicial;
+    }
+
+    public void setDataInicial(String dataInicial) {
+        DateFormat formatador = new SimpleDateFormat("dd/MM/yyyy");
+
+        try {
+            this.dataInicial= formatador.parse(dataInicial);
+        } catch (ParseException e) {
+
+        }
+    }
+
+    public Date getDataFinal() {
+        return dataFinal;
+    }
+
+    public void setDataFinal(String dataFinal) {
+        DateFormat formatador = new SimpleDateFormat("dd/MM/yyyy");
+
+        try {
+            this.dataFinal = formatador.parse(dataFinal);
+        } catch (ParseException e) {
+
+        }
+    }
+
+    public String getCorretor() {
+        return corretor;
+    }
+
+    public void setCorretor(String corretor) {
+        this.corretor = corretor;
+    }
+
+    public String getTipoContrato() {
+        return tipoContrato;
+    }
+
+    public void setTipoContrato(String tipoContrato) {
+        this.tipoContrato = tipoContrato;
+    }
+    
+    
+    
+}
