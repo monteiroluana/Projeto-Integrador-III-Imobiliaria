033c8bbab4cd2b274b27ab8fd4c0d15009161f5a
jonas-ribeiro <jonas.aribeiro@outlook.com>
2018-06-05 17:07:50 -0300

    Validador cpf OK

--- imob/src/main/java/br/com/devolution/model/validadores/ValidadorCliente.java
+++ imob/src/main/java/br/com/devolution/model/validadores/ValidadorCliente.java
@@ -9,6 +9,7 @@ import br.com.devolution.dao.DaoCliente;
 import br.com.devolution.exceptions.ClienteException;
 import br.com.devolution.model.Cliente;
 import br.com.devolution.model.Usuario;
+import java.util.InputMismatchException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -20,8 +21,6 @@ import java.util.regex.Pattern;
 public class ValidadorCliente {
 
     public static boolean validar(Cliente cliente) throws ClienteException, Exception {
-
-        
 
         if (cliente == null) {
             throw new ClienteException("Não foi informado um cliente");
@@ -51,8 +50,15 @@ public class ValidadorCliente {
 
         if (cliente.getTelefone() == null || "".equals(cliente.getTelefone())) {
             throw new ClienteException("É necessário informar o Telefone");
-        }               
-        
+        }
+
+        if (cliente.getCpf() != null) {
+            if (!validarCPF(cliente)) {
+                throw new ClienteException("É necessário informar o Telefone");
+            }
+
+        }
+
         return true;
     }
 
@@ -88,6 +94,67 @@ public class ValidadorCliente {
         }
 
         return true;
+    }
+
+    public static boolean validarCPF(Cliente cliente) throws ClienteException, Exception {
+
+        if (cliente.getCpf().equals("00000000000") || cliente.getCpf().equals("11111111111")
+                || cliente.getCpf().equals("22222222222") || cliente.getCpf().equals("33333333333")
+                || cliente.getCpf().equals("44444444444") || cliente.getCpf().equals("55555555555")
+                || cliente.getCpf().equals("66666666666") || cliente.getCpf().equals("77777777777")
+                || cliente.getCpf().equals("88888888888") || cliente.getCpf().equals("99999999999")
+                || (cliente.getCpf().length() != 11)) {
+            return (false);
+        }
+
+        char dig10, dig11;
+        int sm, i, r, num, peso;
+
+// "try" - protege o codigo para eventuais erros de conversao de tipo (int)
+        try {
+// Calculo do 1o. Digito Verificador
+            sm = 0;
+            peso = 10;
+            for (i = 0; i < 9; i++) {
+// converte o i-esimo caractere do cliente.getCpf() em um numero:
+// por exemplo, transforma o caractere '0' no inteiro 0         
+// (48 eh a posicao de '0' na tabela ASCII)         
+                num = (int) (cliente.getCpf().charAt(i) - 48);
+                sm = sm + (num * peso);
+                peso = peso - 1;
+            }
+
+            r = 11 - (sm % 11);
+            if ((r == 10) || (r == 11)) {
+                dig10 = '0';
+            } else {
+                dig10 = (char) (r + 48); // converte no respectivo caractere numerico
+            }
+// Calculo do 2o. Digito Verificador
+            sm = 0;
+            peso = 11;
+            for (i = 0; i < 10; i++) {
+                num = (int) (cliente.getCpf().charAt(i) - 48);
+                sm = sm + (num * peso);
+                peso = peso - 1;
+            }
+
+            r = 11 - (sm % 11);
+            if ((r == 10) || (r == 11)) {
+                dig11 = '0';
+            } else {
+                dig11 = (char) (r + 48);
+            }
+
+// Verifica se os digitos calculados conferem com os digitos informados.
+            if ((dig10 == cliente.getCpf().charAt(9)) && (dig11 == cliente.getCpf().charAt(10))) {
+                return true;
+            } else {
+                return (false);
+            }
+        } catch (InputMismatchException erro) {
+            return (false);
+        }
     }
 
 }
