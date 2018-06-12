3c15770b6ed3a50a698c1a3704d6ae421a9cca46
Luana Monteiro <luanamonteiropereira95@gmail.com>
2018-06-12 15:31:32 -0300

    filtroBackOffice

---
+++ imob/src/main/java/br/com/devolution/filtro/filtroBackOffice.java
@@ -0,0 +1,177 @@
+
+package br.com.devolution.filtro;
+import br.com.devolution.model.Usuario;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+@WebFilter(filterName = "filtroBackOffice", urlPatterns = {"/CadastroCliente.jsp", "/EditarCliente.jsp", "/ListarClientes.jsp", "/CadastroImovel.jsp", "/EditarImovel.jsp", "/ListarImoveis.jsp"})
+public class filtroBackOffice implements Filter {
+    
+    private static final boolean debug = true;
+ 
+    private FilterConfig filterConfig = null;
+    
+    public filtroBackOffice() {
+    }    
+    
+    private void doBeforeProcessing(ServletRequest request, ServletResponse response)
+            throws IOException, ServletException {
+        if (debug) {
+            log("filtroBackOffice:DoBeforeProcessing");
+        }
+
+       
+    }    
+    
+    private void doAfterProcessing(ServletRequest request, ServletResponse response)
+            throws IOException, ServletException {
+        if (debug) {
+            log("filtroBackOffice:DoAfterProcessing");
+        }
+
+       
+    }
+
+   
+    public void doFilter(ServletRequest request, ServletResponse response,
+            FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        HttpSession sessao = httpRequest.getSession();
+        
+        Usuario usuarioLogado = (Usuario) sessao.getAttribute("usuAutenticado");
+        String cargo = usuarioLogado.getCargo();
+
+        if (cargo.equals("Backoffice")){
+            chain.doFilter(request, response);
+        } else {
+            httpResponse.sendRedirect(httpRequest.getContextPath() + "/autorizacao.jsp");
+        }
+        
+        
+        if (debug) {
+            log("filtroBackOffice:doFilter()");
+        }
+        
+        doBeforeProcessing(request, response);
+        
+        Throwable problem = null;
+        try {
+            //chain.doFilter(request, response);
+        } catch (Throwable t) {
+           
+            problem = t;
+            t.printStackTrace();
+        }
+        
+       
+        if (problem != null) {
+            if (problem instanceof ServletException) {
+                throw (ServletException) problem;
+            }
+            if (problem instanceof IOException) {
+                throw (IOException) problem;
+            }
+            sendProcessingError(problem, response);
+        }
+    }
+
+    
+    public FilterConfig getFilterConfig() {
+        return (this.filterConfig);
+    }
+
+   
+    public void setFilterConfig(FilterConfig filterConfig) {
+        this.filterConfig = filterConfig;
+    }
+
+   
+    public void destroy() {        
+    }
+
+   
+    public void init(FilterConfig filterConfig) {        
+        this.filterConfig = filterConfig;
+        if (filterConfig != null) {
+            if (debug) {                
+                log("filtroBackOffice:Initializing filter");
+            }
+        }
+    }
+
+  
+    @Override
+    public String toString() {
+        if (filterConfig == null) {
+            return ("filtroBakOffice()");
+        }
+        StringBuffer sb = new StringBuffer("filtroBackOffice(");
+        sb.append(filterConfig);
+        sb.append(")");
+        return (sb.toString());
+    }
+    
+    private void sendProcessingError(Throwable t, ServletResponse response) {
+        String stackTrace = getStackTrace(t);        
+        
+        if (stackTrace != null && !stackTrace.equals("")) {
+            try {
+                response.setContentType("text/html");
+                PrintStream ps = new PrintStream(response.getOutputStream());
+                PrintWriter pw = new PrintWriter(ps);                
+                pw.print("<html>\n<head>\n<title>Error</title>\n</head>\n<body>\n"); //NOI18N
+
+                // PENDING! Localize this for next official release
+                pw.print("<h1>The resource did not process correctly</h1>\n<pre>\n");                
+                pw.print(stackTrace);                
+                pw.print("</pre></body>\n</html>"); //NOI18N
+                pw.close();
+                ps.close();
+                response.getOutputStream().close();
+            } catch (Exception ex) {
+            }
+        } else {
+            try {
+                PrintStream ps = new PrintStream(response.getOutputStream());
+                t.printStackTrace(ps);
+                ps.close();
+                response.getOutputStream().close();
+            } catch (Exception ex) {
+            }
+        }
+    }
+    
+    public static String getStackTrace(Throwable t) {
+        String stackTrace = null;
+        try {
+            StringWriter sw = new StringWriter();
+            PrintWriter pw = new PrintWriter(sw);
+            t.printStackTrace(pw);
+            pw.close();
+            sw.close();
+            stackTrace = sw.getBuffer().toString();
+        } catch (Exception ex) {
+        }
+        return stackTrace;
+    }
+    
+    public void log(String msg) {
+        filterConfig.getServletContext().log(msg);        
+    }
+    
+}
